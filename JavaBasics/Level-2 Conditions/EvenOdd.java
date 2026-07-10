0866775676e38aaf60c84f7e4cdcd35798806994
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-10 16:56:09 +0530

    Enhance EvenOdd program: implement user input for number evaluation

--- JavaBasics/Level-2 Conditions/EvenOdd.java
+++ JavaBasics/Level-2 Conditions/EvenOdd.java
@@ -2,7 +2,19 @@ import java.util.*;
 public class EvenOdd {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter Your Number: ");
         int num = sc.nextInt();
+
+        if( num % 2 == 0){
+            System.out.println("The number " + num + " is Even ");
+        }
+        else{
+            System.out.println("The number " + num + " is Odd ");
+        }
+
+        sc.close();
+
     }
+
 }
