6b49ea49cbcca5f741e8b487f5a5cd1774850e9d
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-10 23:18:52 +0530

    Fix Temperature program: change input method from nextInt() to nextDouble() for accurate temperature reading Add Time program: implement functionality to convert seconds into hours, minutes, and seconds

--- JavaBasics/Level-1 WarmUp/Temperature.java
+++ JavaBasics/Level-1 WarmUp/Temperature.java
@@ -5,7 +5,7 @@ public class Temperature {
     public static void main(String [] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter temperature in Celsius: ");
-    double C = sc.nextInt();
+    double C = sc.nextDouble();
 
     double F = (C * 9/5) + 32;
     System.out.println("The temperature is: " + F + "°F");
---
+++ JavaBasics/Level-1 WarmUp/Time.java
@@ -0,0 +1,20 @@
+// Write a program to read seconds and convert them into hours, minutes and seconds.
+
+import java.util.*;
+public class Time {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Seconds: ");
+        int S = sc.nextInt();
+
+        int hour = S / 3600;
+        int remainingSeconds = S % 3600;
+        int minute = remainingSeconds / 60;
+        int second = remainingSeconds % 60;
+        
+    
+        System.out.println("There are "+ hour + " Hours "+ minute + " Minutes and " + second + " Seconds");
+
+
+    }
+}
