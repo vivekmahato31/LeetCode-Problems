d70bfbf08c56857c0cd036175150377c490ed714
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-12 12:44:32 +0530

    Add NaturalNum program: implement functionality to display all natural numbers from 1 to n based on user input

---
+++ JavaBasics/Level-3 Loops/NaturalNum.java
@@ -0,0 +1,26 @@
+// Write a program to display all the natural numbers from 1 to n. (n is user input).
+import java.util.*;
+
+public class NaturalNum {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        //edge case
+        if(n < 0){
+            System.out.println("Please enter a positive integer greater than 0."); 
+        }
+        else if( n == 0){
+            System.out.println(n + " Is not a Natural Number.");
+        }
+        //edge case
+        else{
+            System.out.println("The all natural numbers for 1 to " + n + " are: ");
+            for( int i = 1; i <= n ; i++){
+            System.out.println(i);
+        }
+        }
+        sc.close();
+    }
+}
