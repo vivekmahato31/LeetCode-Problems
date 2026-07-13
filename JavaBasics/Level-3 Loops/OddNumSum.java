1b43570c32eb9ff0b514fed5bf190d20d3e11d41
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-13 10:58:22 +0530

    Add OddNumSum program: implement functionality to calculate the sum of odd numbers from 1 to n

---
+++ JavaBasics/Level-3 Loops/OddNumSum.java
@@ -0,0 +1,25 @@
+//  Write a program to find the sum of all odd numbers from 1 to n.
+
+import java.util.*;
+
+public class OddNumSum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        
+        if (n <= 0) {
+            System.out.println("The number should be greater than zero.");
+        }
+        else {
+            System.out.println("The Sum of all even numbers from 1 to " + n + " are: ");
+            for (int i = 1; i <= n; i += 2){
+                    sum += i;
+            }
+            System.out.println(sum);
+        }
+        sc.close();
+    }
+}
+
