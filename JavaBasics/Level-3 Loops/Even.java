5f33af0775fd9801e7de6c3296c0b2e7bf317d4c
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-13 10:34:25 +0530

    Refactor Even program: improve code formatting for better readability

--- JavaBasics/Level-3 Loops/Even.java
+++ JavaBasics/Level-3 Loops/Even.java
@@ -18,10 +18,10 @@ public class Even {
             System.out.println("All even number from 1 to " + n + " are: ");
             
             for ( int i = 1; i <= n ; i++ ){
-            if( i % 2 == 0){
-                System.out.println(i);
+                if( i % 2 == 0){
+                    System.out.println(i);
+                }
             }
-        }
         }
         sc.close();
     }
