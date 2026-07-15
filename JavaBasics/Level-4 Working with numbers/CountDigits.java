ecfc10ee7f96255771b4435bb35bd9da9d8e0302
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-15 21:50:02 +0530

    Remove unnecessary blank lines in CountDigits program for cleaner code

--- JavaBasics/Level-4 Working with numbers/CountDigits.java
+++ JavaBasics/Level-4 Working with numbers/CountDigits.java
@@ -4,9 +4,7 @@ import java.util.*;
 
 public class CountDigits {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         n = Math.abs(n);
@@ -23,7 +21,6 @@ public class CountDigits {
                 n = n / 10;
             }
             System.out.println("The number of digits is: " + count);
-
         }
         sc.close();
     }
