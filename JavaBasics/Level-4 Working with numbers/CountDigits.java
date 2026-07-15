45911cf3308b73ee8e2a3fde562ccd2d6499bd25
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-15 21:46:42 +0530

    Improve input prompts and enhance edge case handling in CountDigits program

--- JavaBasics/Level-4 Working with numbers/CountDigits.java
+++ JavaBasics/Level-4 Working with numbers/CountDigits.java
@@ -3,11 +3,32 @@
 import java.util.*;
 
 public class CountDigits {
-    public static void main(String [] args){
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n: ");
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        // Edge Case 
+        if (n == 0) {
+            System.out.println("The number of digits is: 1");
+            sc.close();
+            return;
+        }
+
+        // Edge Case 
+        n = Math.abs(n);
+
         int count = 0;
-        
-        
+
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+
+        System.out.println("The number of digits is: " + count);
+
+        sc.close();
     }
 }
