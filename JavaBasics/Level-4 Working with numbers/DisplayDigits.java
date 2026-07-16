b28a00e7c705a6659a69fc1f108205c58e2be197
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-16 22:47:04 +0530

    Add DisplayDigits program: implement functionality to display each digit of a number on a new line

---
+++ JavaBasics/Level-4 Working with numbers/DisplayDigits.java
@@ -0,0 +1,12 @@
+//  Write a program to display all the digits of a number n (one per line).
+
+import java.util.*;
+
+public class DisplayDigits {
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+
+    }
+}
