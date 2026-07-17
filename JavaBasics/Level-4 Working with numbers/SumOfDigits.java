e37ab877c3a2a76112794b7795c780f69c4563fd
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-17 14:03:30 +0530

    Add SumOfDigits program: implement functionality to calculate the sum of all digits of a number

---
+++ JavaBasics/Level-4 Working with numbers/SumOfDigits.java
@@ -0,0 +1,13 @@
+// Write a program to find the sum of all digits of a number n.
+
+import java.util.*;
+
+public class SumOfDigits {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        sc.nextInt();
+
+        
+    }   
+}
