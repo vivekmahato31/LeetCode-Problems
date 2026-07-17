59de72d716f5d88c09058fbae31869b97d695760
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-17 14:47:40 +0530

    Enhance SumOfDigits program: add handling for negative input and improve output formatting

--- JavaBasics/Level-4 Working with numbers/SumOfDigits.java
+++ JavaBasics/Level-4 Working with numbers/SumOfDigits.java
@@ -6,8 +6,23 @@ public class SumOfDigits {
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n: ");
-        sc.nextInt();
+        int n =sc.nextInt();
+        n = Math.abs(n);
+        int digit = 0 ;
+        int sum = 0 ;
 
-        
+        if ( n == 0){
+            System.out.println("0");
+        }
+        else {
+            System.out.println("The sum of all digits of a number "+ n + " are: ");
+            while ( n > 0){
+                digit = n % 10;
+                n = n/10;
+                sum = sum + digit;
+            }
+            System.out.println(sum);
+        }
+        sc.close();
     }   
 }
