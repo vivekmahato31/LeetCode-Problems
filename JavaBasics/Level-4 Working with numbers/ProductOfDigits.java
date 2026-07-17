9d3acae6d6e4a4fa8bd6dea5c7137e9726a1cc14
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-17 14:58:10 +0530

    Add ProductOfDigits program: implement functionality to calculate the product of all digits of a number

---
+++ JavaBasics/Level-4 Working with numbers/ProductOfDigits.java
@@ -0,0 +1,30 @@
+// Write a program to find the product of all digits of a number n.
+
+import java.util.*;
+
+public class ProductOfDigits {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n =sc.nextInt();
+        int original = n;
+        n = Math.abs(n);
+        int product = 1 ;
+
+        if ( n == 0){
+            System.out.println("The Product of all digits of a number "+ original + " is: ");
+            System.out.println("0");
+        }
+        else {
+                System.out.println("The Product of all digits of a number "+ original + " is: ");
+            while ( n > 0){
+                int digit = n % 10;
+                n = n/10;
+                product = product * digit;
+            }
+            System.out.println(product);
+        }
+        sc.close();
+    }   
+}
+
