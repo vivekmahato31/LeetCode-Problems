aef0566e4ff0c6c9fd32c98b3859aa1098c427d5
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-18 23:59:56 +0530

    Add LargestDigit program: implement input handling to find the largest digit in a number

--- JavaBasics/Level-4 Working with numbers/LargestDigit.java
+++ JavaBasics/Level-4 Working with numbers/LargestDigit.java
@@ -1,5 +1,12 @@
 // Write a program to find the largest digit in a number n
+import java.util.*;
 
 public class LargestDigit {
-    
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+        int digit = 0;
+        n = Math.abs(n);    
+    }
 }
