948b5766223c7ab0e9f94656192a89acc7c7ce5d
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-18 23:57:01 +0530

    Add ReverseNum program: implement functionality to reverse a number

---
+++ JavaBasics/Level-4 Working with numbers/ReverseNum.java
@@ -0,0 +1,26 @@
+//  Write a program to reverse a number n.
+
+import java.util.*;
+
+public class ReverseNum {
+     public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+        int digit = 0;
+        n = Math.abs(n);
+
+        if ( n == 0 ){
+            System.out.println("0");
+        }
+        else{
+            System.out.println("The digits of a number are : ");
+            while ( n > 0){
+                digit = n %10;
+                System.out.println(digit);
+                n = n/10;
+            } 
+        }  
+        sc.close();
+    }
+}
