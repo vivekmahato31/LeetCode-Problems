44c28b19eceba9d2f36ffeee64b93c30c6d3e41e
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-24 23:49:22 +0530

    Refactor LargestDigit program: streamline logic for finding the largest digit

--- JavaBasics/Level-4 Working with numbers/LargestDigit.java
+++ JavaBasics/Level-4 Working with numbers/LargestDigit.java
@@ -3,12 +3,30 @@
 import java.util.Scanner;
 
 public class LargestDigit {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
+        n = Math.abs(n);
+        int largest = 0;
 
+        if (n == 0) {
+            largest = 0;
+        } 
+        else {
+            while (n > 0) {
+                
+                int digit = n % 10;
+
+                if (digit > largest) {
+                    largest = digit;
+                }
+
+                n = n / 10;
+            }
+        }
 
         System.out.println("Largest digit = " + largest);
 
